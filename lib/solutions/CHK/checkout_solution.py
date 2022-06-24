5f43fed76207f136c30205def5f497cd2d76f068
sbx_user1051 <sbx_user1051@169.254.187.73>
2022-06-24 12:37:29 +0000

    Fri Jun 24 12:37:29 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,7 +10,7 @@ def checkout(skus):
         return 0
     if len(skus[0]) == 0:
         return 0
-    if  skus.islower() or not skus.isalpha():
+    if  not skus.isupper() or not skus.isalpha():
         return -1
     # Get total number of each amount
 
@@ -51,7 +51,7 @@ def checkout(skus):
     counts[21] = np.floor(counts[21]/3)*130 + np.floor((counts[21]%3)/2)*90+ ((counts[21]%3) % 2) * 50
     counts[22] = counts[22]*20
 
-    stxyz = (counts[18]+ counts[19]+ counts[22]+ counts[23]+ counts[24]+ counts[25])
+    stxyz = (counts[18]+ counts[19]+ counts[23]+ counts[24]+ counts[25])
 
     remainder = stxyz % 3
     s = 0
@@ -88,4 +88,5 @@ def checkout(skus):
     total = np.sum(counts)+np.floor(stxyz/3)*45
 
     return total
+
 
