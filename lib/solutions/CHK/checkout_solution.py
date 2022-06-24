26867355206b90ffb01d2c8908d64dbdae4f62a2
sbx_user1051 <sbx_user1051@169.254.187.73>
2022-06-24 11:21:39 +0000

    Fri Jun 24 11:21:39 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,6 +7,7 @@ def checkout(skus):
     b = 0
     c = 0
     d = 0
+    # Get total number of each amount
     for letter in skus:
         if letter == 'A':
             a +=1
@@ -18,8 +19,15 @@ def checkout(skus):
             d +=1
         else:
             return -1
-    a_tot = np.floor(a/3)*130+a%3 *50
+
+    # Calculate totals for each
+    a_tot = np.floor(a/3)*130+ a % 3 * 50 # get number of complete sets of 3 and add deal, get remainder to calculate total
+    b_tot = np.floor(b/2)*45 + b % 2 * 30 # get number of complete sets of 2 and add deal, get remainder to calculate total
     c_tot = c*20
     d_tot = d*15
+
+    total = a_tot+b_tot+c_tot+d_tot
+
+    return total
 
 
