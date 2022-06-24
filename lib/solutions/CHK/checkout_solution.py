c225635c1661c980ac478d53f99aa4748e20c06a
sbx_user1051 <sbx_user1051@169.254.187.73>
2022-06-24 11:17:14 +0000

    Fri Jun 24 11:17:14 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,22 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     total = 0
-    a= 0
-    b= 0
-    c= 0
-    d= 0
-    for i in range(len(skus)):
-        if 
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    for letter in skus:
+        if letter == 'A':
+            a +=1
+        elif letter == ' B':
+            b +=1
+        elif letter == ' C':
+            c +=1
+        elif letter == ' D':
+            d +=1
+        else:
+            return -1
+    
 
